9bd0d6a67a31593224fd14e079c1d8dc6f5b35e1
Arthur Fernandes Vieira <arthur.fv324@gmail.com>
2025-05-02 23:10:53 -0300

    feat: página painel de controle

---
+++ src/pg_painel_controle.py
@@ -0,0 +1,87 @@
+import flet as ft
+
+from widgets.barra_lateral import barra_lateral
+
+def pg_painel_controle(page: ft.Page):
+    page.title = "Painel de Controle"
+    page.window.center()
+    page.bgcolor = ft.Colors.BLUE_GREY_900
+    page.padding = 5
+    page.update()
+
+    page_routes = (
+        '/login',
+        '/cadastro_vendas',
+        '/revisao_vendas'
+    )
+
+    container_vendas = ft.Container(
+        content=ft.Column(
+                    controls=[
+                        ft.Divider(thickness=0.01, height=5),
+                        ft.Text('VENDAS', size=30, theme_style=ft.TextThemeStyle.LABEL_LARGE),
+                        ft.Divider(),
+                        ft.ElevatedButton(
+                            text='CADASTRAR',
+                            height=80, 
+                            width=150, 
+                            style=ft.ButtonStyle(shape=ft.ContinuousRectangleBorder(radius=12)),
+                            on_click=lambda _: page.go(page_routes[1])
+                            ),
+                        ft.ElevatedButton(
+                            text='REVISAR', 
+                            height=80, 
+                            width=150, 
+                            style=ft.ButtonStyle(shape=ft.ContinuousRectangleBorder(radius=12)),
+                            on_click=lambda _ : page.go(page_routes[2])
+                            )
+                        ],
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    spacing=35,
+        ),
+        width=250
+    )
+
+    container_outras_saidas = ft.Container(
+        content=ft.Column(
+                controls=[
+                        ft.Divider(thickness=0.01, height=5),
+                        ft.Text('OUTRAS SAÍDAS', size=30, theme_style=ft.TextThemeStyle.LABEL_LARGE),
+                        ft.Divider(),
+                        ft.ElevatedButton(text='CADASTRAR', height=80, width=150, style=ft.ButtonStyle(shape=ft.ContinuousRectangleBorder(radius=12))),
+                        ft.ElevatedButton(text='REVISAR', height=80, width=150, style=ft.ButtonStyle(shape=ft.ContinuousRectangleBorder(radius=12)))
+                        ],
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    spacing=35,
+        ),
+        width=250
+    )
+    
+    container_faruramento = ft.Container(
+        content=ft.Column(
+                controls=[
+                        ft.Divider(thickness=0.01, height=5),
+                        ft.Text('FATURAMENTO', size=30, theme_style=ft.TextThemeStyle.LABEL_LARGE),
+                        ft.Divider(),
+                        ft.ElevatedButton(text='VER', height=80, width=150, style=ft.ButtonStyle(shape=ft.ContinuousRectangleBorder(radius=12))),
+                        ],
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    spacing=35,
+        ),
+        width=250,
+    )
+
+    layout = ft.Row(
+            controls=[
+                barra_lateral(page.go),
+                container_vendas,
+                ft.VerticalDivider(width=0, trailing_indent=900),
+                container_outras_saidas,
+                ft.VerticalDivider(width=0, trailing_indent=900),
+                container_faruramento,
+                ft.VerticalDivider(width=0, trailing_indent=900),
+
+            ],
+        expand=True
+    )
+    return layout
