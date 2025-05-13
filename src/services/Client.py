264a3d3354d124259384cfb9fb34dc32973cc016
Arthur Fernandes Vieira <arthur.fv324@gmail.com>
2025-05-13 16:33:00 -0300

    feat: login, fetch de vendas e fetch de vendedores

--- src/services/Client.py
+++ src/services/Client.py
@@ -1,7 +1,6 @@
 import asyncpg
 from database.instance import *
 
-from asyncio import sleep
 
 class Client():
 
@@ -24,31 +23,41 @@ class Client():
     async def close(self):
         await self.pool.close()
 
-    async def cadastrar_loja(self, cnpj, nome):
+    async def cadastrar_loja(self, cnpj: str, nome: str):
         async with self.pool.acquire() as conn:
-            conn.execute('INSERT INTO loja(cnpj, nome) VALUES($1, $2)', 
+            await conn.execute('INSERT INTO loja(cnpj, nome) VALUES($1, $2)', 
                          cnpj, 
                          nome
             )
 
-    async def cadastrar_funcionario(self, nome, salario, funcao='', id_gerente=''):
+    async def cadastrar_funcionario(self, 
+                                    nome: str, 
+                                    salario: float, 
+                                    funcao='', 
+                                    id_gerente=''):
         async with self.pool.acquire() as conn:
-            conn.execute('INSERT INTO funcionario(nome, funcao, salario, id_genrente) VALUES($1, $2, $3, $4)',
+            await conn.execute('INSERT INTO funcionario(nome, funcao, salario, id_genrente) VALUES($1, $2, $3, $4)',
                          nome, 
                          funcao, 
                          salario, 
                          id_gerente
             )
             
-    async def cadastrar_loja_trabalho_funcionario(self, id_funcionario, cnpj, inicio=None):
+    async def cadastrar_loja_trabalho_funcionario(self, 
+                                                  id_funcionario: int, 
+                                                  cnpj: str, 
+                                                  inicio=None):
         async with self.pool.acquire() as conn:
-            conn.execute('INSERT INTO tabalha_em(id_funcionario, cnpj, inicio, fim) VALUES($1, $2, $3, NULL)',
+            await conn.execute('INSERT INTO tabalha_em(id_funcionario, cnpj, inicio, fim) VALUES($1, $2, $3, NULL)',
                          id_funcionario,
                          cnpj,
                          inicio
             )
 
-    async def cadastrar_usuario(self, nome, cnpj, senha):
+    async def cadastrar_usuario(self, 
+                                nome: str, 
+                                cnpj: str, 
+                                senha: str):
         async with self.pool.acquire() as conn:
             await conn.execute('INSERT INTO usuario(nome, cnpj, senha) VALUES($1, $2, $3)',
                          nome,
@@ -56,25 +65,34 @@ class Client():
                          senha
             )
     
-    async def cadastrar_venda(self, montante, id_vendedor):
+    async def cadastrar_venda(self, 
+                              montante: float, 
+                              id_vendedor: int):
         async with self.pool.acquire() as conn:
-            conn.execute('INSERT INTO venda(montante, id_vendedor) VALUES($1, $2)',
+            await conn.execute('INSERT INTO venda(montante, id_vendedor) VALUES($1, $2)',
                          montante,
                          id_vendedor
             )
+
             
-    async def login(self, nome, senha):
+    async def login(self, 
+                    nome: str, 
+                    senha: str) -> bool:
         async with self.pool.acquire() as conn:
-            await conn.fetch('SELECT 1 FROM usuarios WHERE nome = $1 AND senha = $2', 
+            resultado = await conn.fetchval('SELECT 1 FROM usuarios WHERE nome = $1 AND senha = $2', 
                              nome, 
                              senha
                     )
+        return resultado != None
 
-    def fetch_vendas_concluidas():
-        pass
+    async def fetch_vendas_concluidas_hoje(self) -> list[asyncpg.Record]:
+        async with self.pool.acquire() as conn:
+            resultado = await conn.fetch('SELECT * venda WHERE data_venda = CURRENT_DATE')
+        return resultado
     
     async def fetch_vendedores_ativos(self, cnpj: str):
         async with self.pool.acquire() as conn:
-            linhas = await conn.fetch('SELECT * FROM trabalha_em WHERE cnpj = $1 AND fim IS NULL', 
+            records = await conn.fetch('SELECT * FROM trabalha_em WHERE cnpj = $1 AND fim IS NULL', 
                                       cnpj
                             )
+            
